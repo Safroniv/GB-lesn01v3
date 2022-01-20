8673dd0d9b150677373e22315496d54c0ba901c2
Safroniv <safroniv@mail.ru>
2022-01-20 19:37:28 +0300

    Добавьте файлы проекта.

---
+++ GB-lesn01v3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GB_lesn01v3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
