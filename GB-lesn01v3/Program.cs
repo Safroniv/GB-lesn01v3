ac001a4a32d891e6ecca47ee01ace200f2dd90d4
Safroniv <safroniv@mail.ru>
2022-01-23 13:32:38 +0300

    Запрос имени в консоли

--- GB-lesn01v3/Program.cs
+++ GB-lesn01v3/Program.cs
@@ -6,10 +6,12 @@ namespace GB_lesn01v3
     {
         static void Main()
         {
+            Console.WriteLine("Здравствуйте, введите пожалуйста Ваше имя:");
             //Запрос ввода имени
             string nameUsr = Console.ReadLine();
             //Вывод приветствия
             Console.WriteLine("Привет, " + nameUsr +", сегодня " + DateTime.Now.ToShortDateString());
+            
         }
     }
-}
+    }
